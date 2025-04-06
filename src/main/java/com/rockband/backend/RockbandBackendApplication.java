d6470b8a594d8ea5af083d9369a5dfb661267df9
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-04-06 15:44:46 +0200

    Initial Spring Boot project

---
+++ src/main/java/com/rockband/backend/RockbandBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rockband.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockbandBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockbandBackendApplication.class, args);
+	}
+
+}
