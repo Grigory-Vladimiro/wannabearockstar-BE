26dd013e2d13dd72fa72021de253d67e19fe0b50
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-04-14 22:47:19 +0200

    app_update_concert_repository

--- src/main/java/com/rockband/RockbandBackendApplication.java
+++ src/main/java/com/rockband/RockbandBackendApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import com.rockband.model.Album;
+import com.rockband.model.Concert;
 import com.rockband.repository.AlbumRepository;
+import com.rockband.repository.ConcertRepository;
 
 @SpringBootApplication
 public class RockbandBackendApplication {
@@ -15,11 +17,15 @@ public class RockbandBackendApplication {
 		SpringApplication.run(RockbandBackendApplication.class, args);
 	}
 @Bean
-    CommandLineRunner initDatabase(AlbumRepository albumRepository) {
+    CommandLineRunner initDatabase(AlbumRepository albumRepository, ConcertRepository concertRepository) {
         return args -> {
             albumRepository.save(new Album("Highway to Hell", 1979, "img/albums/album1.jpg", "Legendary AC/DC album"));
 			albumRepository.save(new Album("Back in Black", 1980, "img/albums/album2.jpg", "One of the best-selling albums ever"));
 			albumRepository.save(new Album("The Black Album", 1991, "img/albums/album3.jpg", "Metallica's iconic album"));
+
+            concertRepository.save(new Concert("Madrid", "Rock Arena", "2025-05-10"));
+            concertRepository.save(new Concert("Barcelona", "Electric Stage", "2025-06-02"));
+            concertRepository.save(new Concert("Valencia", "Sunset Club", "2025-07-15"));
         };
     }
 }
