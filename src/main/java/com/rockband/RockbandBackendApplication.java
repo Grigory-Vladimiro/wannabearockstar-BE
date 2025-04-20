e2c9fb213663879f9f1b52bda9dc5fd8f29a2483
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-04-20 19:43:41 +0200

    app_update_songs

--- src/main/java/com/rockband/RockbandBackendApplication.java
+++ src/main/java/com/rockband/RockbandBackendApplication.java
@@ -12,7 +12,7 @@ import com.rockband.model.News;
 import com.rockband.repository.AlbumRepository;
 import com.rockband.repository.ConcertRepository;
 import com.rockband.repository.NewsRepository;
-
+import java.util.List;
 import java.time.LocalDate;
 
 @SpringBootApplication
@@ -24,22 +24,30 @@ public class RockbandBackendApplication {
 	}
 
 	@Bean
-	CommandLineRunner initDatabase(
-		AlbumRepository albumRepository,
-		ConcertRepository concertRepository,
-		NewsRepository newsRepository
-	) {
-		return args -> {
+CommandLineRunner initDatabase(
+    AlbumRepository albumRepository,
+    ConcertRepository concertRepository,
+    NewsRepository newsRepository
+) {
+    return args -> {
+        Album album1 = new Album("Highway to Hell", 1979, "img/albums/album1.jpg", "Legendary AC/DC album");
+        album1.setSongs(List.of("Highway to Hell", "Girls Got Rhythm", "If You Want Blood"));
 
-			albumRepository.save(new Album("Highway to Hell", 1979, "img/albums/album1.jpg", "Legendary AC/DC album"));
-			albumRepository.save(new Album("Back in Black", 1980, "img/albums/album2.jpg", "One of the best-selling albums ever"));
-			albumRepository.save(new Album("The Black Album", 1991, "img/albums/album3.jpg", "Metallica's iconic album"));
+        Album album2 = new Album("Back in Black", 1980, "img/albums/album2.jpg", "One of the best-selling albums ever");
+        album2.setSongs(List.of("Hells Bells", "Shoot to Thrill", "Back in Black"));
 
-			concertRepository.save(new Concert("Madrid", "Rock Arena", "2025-05-10"));
-			concertRepository.save(new Concert("Barcelona", "Electric Stage", "2025-06-02"));
-			concertRepository.save(new Concert("Valencia", "Sunset Club", "2025-07-15"));
+        Album album3 = new Album("The Black Album", 1991, "img/albums/album3.jpg", "Metallica's iconic album");
+        album3.setSongs(List.of("Enter Sandman", "Sad but True", "Nothing Else Matters"));
 
-			newsRepository.save(new News("Site launched! Stay tuned for updates.", LocalDate.of(2025, 4, 20)));
-		};
+        albumRepository.save(album1);
+        albumRepository.save(album2);
+        albumRepository.save(album3);
+
+        concertRepository.save(new Concert("Madrid", "Rock Arena", "2025-05-10"));
+        concertRepository.save(new Concert("Barcelona", "Electric Stage", "2025-06-02"));
+        concertRepository.save(new Concert("Valencia", "Sunset Club", "2025-07-15"));
+
+        newsRepository.save(new News("Site launched! Stay tuned for updates.", LocalDate.of(2025, 4, 20)));
+    };
 	}
 }
