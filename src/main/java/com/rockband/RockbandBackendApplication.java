2b517f662697b01f272fb330508a07f456491224
Grigory Vladimiro <grigori.vladimiro@gmail.com>
2025-04-20 18:37:13 +0200

    app_news_update

--- src/main/java/com/rockband/RockbandBackendApplication.java
+++ src/main/java/com/rockband/RockbandBackendApplication.java
@@ -7,8 +7,12 @@ import org.springframework.context.annotation.Bean;
 
 import com.rockband.model.Album;
 import com.rockband.model.Concert;
+import com.rockband.model.News;
 import com.rockband.repository.AlbumRepository;
 import com.rockband.repository.ConcertRepository;
+import com.rockband.repository.NewsRepository;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class RockbandBackendApplication {
@@ -16,16 +20,26 @@ public class RockbandBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RockbandBackendApplication.class, args);
 	}
-@Bean
-    CommandLineRunner initDatabase(AlbumRepository albumRepository, ConcertRepository concertRepository) {
-        return args -> {
-            albumRepository.save(new Album("Highway to Hell", 1979, "img/albums/album1.jpg", "Legendary AC/DC album"));
+
+	@Bean
+	CommandLineRunner initDatabase(
+		AlbumRepository albumRepository,
+		ConcertRepository concertRepository,
+		NewsRepository newsRepository
+	) {
+		return args -> {
+			
+			albumRepository.save(new Album("Highway to Hell", 1979, "img/albums/album1.jpg", "Legendary AC/DC album"));
 			albumRepository.save(new Album("Back in Black", 1980, "img/albums/album2.jpg", "One of the best-selling albums ever"));
 			albumRepository.save(new Album("The Black Album", 1991, "img/albums/album3.jpg", "Metallica's iconic album"));
 
-            concertRepository.save(new Concert("Madrid", "Rock Arena", "2025-05-10"));
-            concertRepository.save(new Concert("Barcelona", "Electric Stage", "2025-06-02"));
-            concertRepository.save(new Concert("Valencia", "Sunset Club", "2025-07-15"));
-        };
-    }
+		
+			concertRepository.save(new Concert("Madrid", "Rock Arena", "2025-05-10"));
+			concertRepository.save(new Concert("Barcelona", "Electric Stage", "2025-06-02"));
+			concertRepository.save(new Concert("Valencia", "Sunset Club", "2025-07-15"));
+
+			
+			newsRepository.save(new News("Site launched! Stay tuned for updates.", LocalDate.of(2025, 4, 20)));
+		};
+	}
 }
